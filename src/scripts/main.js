4f9a88ff77f200b0768d775e872311c89dd07853
Yaroslav Kolyagin <yaroslavkolyagin@gmail.com>
2022-06-01 16:41:54 +0300

    Добавлен layot-5, ссылки на якоря.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,8 @@
 new WOW().init();
+var $page = $('html, body');
+$('a[href*="#"]').click(function () {
+	$page.animate({
+		scrollTop: $($.attr(this, 'href')).offset().top
+	}, 300);
+	return false;
+});
